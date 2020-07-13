fe038dcc4e15527bc44e7ced56eb5d766a695d62
PopeyeIT <igorserov28@gmail.com>
2020-07-13 15:37:05 +0300

    Updates

---
+++ precache-manifest.1684fd916e9bcf20f4cbb1abedb524de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56cee7c6f5e01c6af695cc7b183f1a02",
+    "url": "/goit-react-hw-03-components/index.html"
+  },
+  {
+    "revision": "006cade5aac6f27fb969",
+    "url": "/goit-react-hw-03-components/static/css/main.5f82d52d.chunk.css"
+  },
+  {
+    "revision": "2a816a29222ae1c71da0",
+    "url": "/goit-react-hw-03-components/static/js/2.f0209e38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-03-components/static/js/2.f0209e38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006cade5aac6f27fb969",
+    "url": "/goit-react-hw-03-components/static/js/main.3cf25301.chunk.js"
+  },
+  {
+    "revision": "d9d280912fe4a77499bf",
+    "url": "/goit-react-hw-03-components/static/js/runtime-main.0247b7f8.js"
+  }
+]);
